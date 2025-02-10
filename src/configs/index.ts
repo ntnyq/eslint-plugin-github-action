06a413bfb12d90202c82c32b321f64997ee80b63
ntnyq <ntnyq13@gmail.com>
2025-02-10 19:41:45 +0800

    feat!: remove deprecated function `createRecommendedConfig`

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -11,16 +11,6 @@ export type CreateConfigOptions = Omit<Linter.Config, 'rules'> & {
 }
 
 /**
- * @deprecated
- */
-export interface RecommendedOptions extends Linter.Config {
-  /**
-   * Overrides rules.
-   */
-  overridesRules?: RulesWithPluginName
-}
-
-/**
  * Create a ESLint config.
  *
  * @param options - ESLint Linter.Config with type support.
@@ -42,45 +32,6 @@ export function createConfig(options: CreateConfigOptions = {}) {
     languageOptions: {
       ...(options.languageOptions || {}),
       parser: yamlParser,
-    },
-  }
-  return config
-}
-
-/**
- * Create recommended config in flat style.
- *
- * @param options - Create recommended config {@link RecommendedOptions}.
- * @returns flat recommended config.
- * @deprecated use {@link createConfig} instead
- */
-export function createRecommendedConfig(options: RecommendedOptions = {}) {
-  const config: Linter.Config = {
-    ...options,
-
-    // Overrides
-    name: options.name || 'github-action/recommended',
-    files: options.files || ['**/.github/workflows/*.y?(a)ml'],
-    ignores: options.ignores || ['!**/.github/workflows/*.y?(a)ml'],
-    languageOptions: {
-      ...(options.languageOptions || {}),
-      parser: yamlParser,
-    },
-    plugins: {
-      ...(options.plugins || {}),
-
-      /* v8 ignore start */
-      get 'github-action'() {
-        return plugin
-      },
-      /* v8 ignore stop */
-    },
-    rules: {
-      'github-action/no-invalid-key': 'error',
-      'github-action/prefer-file-extension': 'error',
-      'github-action/require-action-name': 'error',
-
-      ...(options.overridesRules || {}),
     },
   }
   return config
