44201b35d9bb05ecbb921a027c032041955c50ae
Vladislav Shkodin <smashercosmo@gmail.com>
2026-08-28 22:57:35 +0300

    feat: add `allowEmpty` option to the `no-top-level-permissions` rule

--- src/rules/no-top-level-permissions.ts
+++ src/rules/no-top-level-permissions.ts
@@ -1,9 +1,18 @@
-import { createESLintRule } from '../utils'
+import { createESLintRule, resolveOptions } from '../utils'
 import type { YAMLAst } from '../types/yaml'
 
 export const RULE_NAME = 'no-top-level-permissions'
 export type MessageIds = 'disallowTopLevelPermissions'
-export type Options = []
+export type Options = [
+  {
+    /**
+     * Allow empty permissions block
+     */
+    allowEmpty?: boolean
+  },
+]
+
+const defaultOptions = { allowEmpty: false } satisfies Options[0]
 
 export default createESLintRule<Options, MessageIds>({
   name: RULE_NAME,
@@ -13,7 +22,21 @@ export default createESLintRule<Options, MessageIds>({
       recommended: false,
       description: 'disallow using top-level permissions.',
     },
-    schema: [],
+    schema: [
+      {
+        type: 'object',
+        additionalProperties: false,
+        properties: {
+          allowEmpty: {
+            title: 'Allow empty permissions block.',
+            description:
+              'Some people consider putting an empty permissions block at the top level a good practice in case GitHub token has permissions (enabled intentionally or by accident), which are not relevant for the current workflow.',
+            type: 'boolean',
+          },
+        },
+      },
+    ],
+    defaultOptions: [defaultOptions],
     messages: {
       disallowTopLevelPermissions: 'Disallow using top level permissions.',
     },
@@ -22,6 +45,19 @@ export default createESLintRule<Options, MessageIds>({
     return {
       'Program > YAMLDocument > YAMLMapping > YAMLPair[key.value=permissions]':
         (node: YAMLAst.YAMLPair) => {
+          const { allowEmpty = false } = resolveOptions(
+            context.options,
+            defaultOptions,
+          )
+
+          if (
+            allowEmpty &&
+            node.value?.type === 'YAMLMapping' &&
+            node.value.pairs.length === 0
+          ) {
+            return
+          }
+
           context.report({
             node,
             loc: node.loc,
